51c650720f0c5a1e13c0d7a5bfc532b81d425cdc
Robert Collins <collinsrj@gmail.com>
2015-02-16 21:25:40 +0000

    Got some buttons working.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
