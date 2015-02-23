f9127af56b032dc7d4b6ba181555ce0a5caeb110
Robert Collins <collinsrj@gmail.com>
2015-02-23 22:58:46 +0000

    Added square root button and implemented the single argument performOperation method.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         case "−" : performOperation {$1 - $0}
         case "+" : performOperation {$0 + $1}
         case "÷" : performOperation {$1 / $0}
-            
+        case "√" : performOperation {sqrt($0)}
         default : break
         }
     }
@@ -32,6 +32,13 @@ class ViewController: UIViewController {
     func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: (Double) -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
             enter()
         }
     }
